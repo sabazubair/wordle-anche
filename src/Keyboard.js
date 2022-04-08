81c28d842fbd2ee47a9f051c66fcc1eefd8fd5e5
Saba Zubair <zubairs19@protonmail.com>
2022-04-08 16:02:54 -0400

    Used branching structure to capture all edge cases, implemented enter and delete functionality.

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -2,27 +2,43 @@ import { useState, useEffect } from 'react';
 import { BsSlashCircleFill } from 'react-icons/bs';
 
 export default function Keyboard(props) {
-  const [str, setStr] = useState("");
-  const vals = ["Q", "W", "E", "R", "T", "Y","U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter", "Delete", "Z", "X", "C", "V", "B", "N", "M"]
+  const keyboard_vals = ["Q", "W", "E", "R", "T", "Y","U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter", "Delete", "Z", "X", "C", "V", "B", "N", "M"]
 
   function handleOnClick(e) {
-    setStr(str + e.target.innerText);
     if(e.target.innerText === 'Delete') {
-      const copy = str;
-      copy.slice(0, str.length-1);
-      setStr(copy);
+      if(props.input.length===0) {
+        return
+      }
+      else {
+        const text = props.input.slice(0, -1);
+        props.setInput(text);
+      }
     }
-    console.log(str);
+    else if(e.target.innerText === 'Enter') {
+      if(props.input.length === 5){
+        props.inputValidator(props.input);
+        const currentWords = [...props.words];
+        currentWords.push(props.input);
+        props.setWords(currentWords);
+        props.setInput("");
+      }
+    } else if (props.input.length === 5){ // if input is 5, 2 options: delete, or enter
+      return;
+    }
+    else {
+      props.setInput(props.input + e.target.innerText);
+    }
   }
 
   useEffect(() => {
-  }, [str]);
+    console.log(props.input);
+  }, [props.input]);
 
   return(
   <div className="keyboard">
     <div className="row">
-      {vals.map((k) => {
-        return <div className="key" onClick={(handleOnClick)}>
+      {keyboard_vals.map((k) => {
+        return <div className="key" onClick={(e) => handleOnClick(e)}>
           {k}
         </div>
       })}
