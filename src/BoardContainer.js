16f03737f99e3e869d825feb85e3d94d4a816469
Saba Zubair <zubairs19@protonmail.com>
2022-04-08 17:43:45 -0400

    created updatedCol function to help determine when to reset grid state

--- src/BoardContainer.js
+++ src/BoardContainer.js
@@ -19,30 +19,46 @@ function BoardContainer(props) {
   const nextRow = useRef(0)
   const nextColumn = useRef(0)
 
+  const updatedColumn = () => {
+    let deletedColumn = null;
+    if(props.input.length < nextColumn.current) {
+      deletedColumn = nextColumn.current - 1;
+      nextColumn.current = props.input.length - 1;
+    }
+    return deletedColumn;
+  }
+
   useEffect(() => {
     if (!props.input) return
 
+    let deletedColumn = updatedColumn();
     const row = nextRow.current
     const column = nextColumn.current
+    console.log("ROW:", row, "COL", column, 'input', props.input);
+
     setGrid((state) => {
       const newState = { ...state, [row]: [ ...state[row] ] }
-      newState[row][column] = props.input
+      newState[row][column] = props.input[column]
+      if(deletedColumn != null) {
+        newState[row][deletedColumn] = "";
+      }
       return newState
     })
-    nextColumn.current += 1
+
+    nextColumn.current = props.input.length;
+    console.log("NEXTCOLUMN.CURRENT", nextColumn.current, "PROPS.INPUT.LENGTH", props.input.length)
     if(column + 1 === columns) {
       nextRow.current += 1
+      console.log("NEXTROW.CURRENT", nextRow.current);
       nextColumn.current = 0
     }
-  }, [props.input])
+  }, [props.input.length])
 
   return (
       <div id="boardrow-container">
-      {[0,1,2,3,4].map((_, index) => {
-        // console.log(grid[index]);
-        return <BoardRow input={grid[index]} />
-      })
-
+        {[0,1,2,3,4].map((idx, index) => {
+          return <BoardRow input={grid[index]} />
+        })
       }
       </div>
   )
