ef975638461105250f3d5f30cfef1a41ce7b4416
Saba Zubair <zubairs19@protonmail.com>
2022-05-24 20:26:49 -0400

    Created tries var to keep track of # of submissions

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import { BsSlashCircleFill } from 'react-icons/bs';
+import 'animate.css';
 
 export default function Keyboard(props) {
   const keyboard_vals = ["Q", "W", "E", "R", "T", "Y","U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter", "Delete", "Z", "X", "C", "V", "B", "N", "M"]
 
   function handleOnClick(e) {
-    if(e.target.innerText === 'Delete') {
+    if(props.tries < 5) {
+      if(e.target.innerText === 'Delete') {
       if(props.input.length===0) {
         return
       }
@@ -13,26 +15,29 @@ export default function Keyboard(props) {
         const text = props.input.slice(0, -1);
         props.setInput(text);
       }
-    }
-    else if(e.target.innerText === 'Enter') {
-      if(props.input.length === 5){
-        props.inputValidator(props.input);
-        const currentWords = [...props.words];
-        currentWords.push(props.input);
-        props.setWords(currentWords);
-        props.setInput("");
       }
-    } else if (props.input.length === 5){ // if input is 5, 2 options: delete, or enter
-      return;
-    }
-    else {
-      props.setInput(props.input + e.target.innerText);
+      else if(e.target.innerText === 'Enter') {
+        if(props.input.length === 5){
+          // console.log("inside if", props.input);
+          props.inputValidator(props.input);
+          const appendToHistory = [...props.history];
+          appendToHistory.push(props.input);
+          props.setHistory(appendToHistory);
+          const addToTries = props.tries + 1;
+          props.setTries(addToTries);
+          props.setInput("");
+        }
+      } else if (props.input.length === 5){ // if input is 5, 2 options: delete, or enter
+        return;
+      }
+      else {
+        const inputArray = [...props.input, {letter: e.target.innerText, color: "#FFFFFF"}]
+        props.setInput(inputArray);
+      }
+    } else {
+      return
     }
   }
-
-  useEffect(() => {
-    console.log(props.input);
-  }, [props.input]);
 
   return(
   <div className="keyboard">
