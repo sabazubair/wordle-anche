79fad4bc5fd6641751f43abcba48b29d75c74d9e
Saba Zubair <zubairs19@protonmail.com>
2022-06-03 14:35:49 -0400

    destructured props

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -2,35 +2,35 @@ import { useState, useEffect } from 'react';
 import { BsSlashCircleFill } from 'react-icons/bs';
 import 'animate.css';
 
-export default function Keyboard(props) {
+export default function Keyboard({tries, setTries, input, setInput, history, setHistory, inputValidator}) {
   const keyboard_vals = ["Q", "W", "E", "R", "T", "Y","U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Enter", "Delete", "Z", "X", "C", "V", "B", "N", "M"]
 
   function handleOnClick(e) {
-    if(props.tries < 5) {
+    if(tries < 5) {
       if(e.target.innerText === 'Delete') {
-        if(props.input.length===0) {
+        if(input.length===0) {
           return
         }
         else {
-          const text = props.input.slice(0, -1);
-          props.setInput(text);
+          const text = input.slice(0, -1);
+          setInput(text);
         }
       }
       else if(e.target.innerText === 'Enter') {
-        if(props.input.length === 5){
+        if(input.length === 5){
           // console.log("inside if", props.input);
-          props.inputValidator(props.input);
-          const appendToHistory = [...props.history];
-          appendToHistory.push(props.input);
-          props.setHistory(appendToHistory);
-          props.setInput([]);
+          inputValidator(input);
+          const appendToHistory = [...history];
+          appendToHistory.push(input);
+          setHistory(appendToHistory);
+          setInput([]);
         }
-      } else if (props.input.length === 5){ // if input is 5, 2 options: delete, or enter
+      } else if (input.length === 5){ // if input is 5, 2 options: delete, or enter
         return;
       }
       else {
-        const inputArray = [...props.input, {letter: e.target.innerText, color: "#FFFFFF"}]
-        props.setInput(inputArray);
+        const inputArray = [...input, {letter: e.target.innerText, color: "#FFFFFF"}]
+        setInput(inputArray);
       }
     } else {
       return
