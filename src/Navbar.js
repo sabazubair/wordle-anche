c110d3f9b421acea0c562bce102b96f6494368d8
Saba Zubair <zubairs19@protonmail.com>
2022-06-07 16:52:43 -0400

    deleted unused icons

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,12 +5,7 @@ import {BsGearFill} from 'react-icons/bs';
 export default function Navbar() {
   return (
     <header id="header-container">
-      <i><AiOutlineQuestionCircle/></i>
-      <h3 className="title">WORDLE</h3>
-      <div>
-        <i><BiBarChartAlt2/></i>
-        <i><BsGearFill/></i>
-      </div>
+      <h3 className="title">Wordle-anch!</h3>
     </header>
   );
 }
