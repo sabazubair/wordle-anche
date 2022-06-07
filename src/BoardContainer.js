8716ed013803f2ba9ba19e809b861eb16c94e75e
Saba Zubair <zubairs19@protonmail.com>
2022-06-07 16:51:54 -0400

    created and configured boardContainer ref to work with gridFall and checkCollision; utilized clearInterval in useEffect to clear setInterval function

--- src/BoardContainer.js
+++ src/BoardContainer.js
@@ -1,34 +1,31 @@
 import BoardRow from './BoardRow';
 import { useState, useEffect, useRef } from 'react';
 
-function BoardContainer({input, inputRow, grid, setGrid}) {
+function BoardContainer({input, inputRow, grid, setGrid, dividerRef}) {
+  const boardContainer = useRef(null);
 
-  const gridFall = () => {
-
-    const gridBody = document.getElementById("boardrow-container");
-    const interval = setInterval(() => {
-      const currentPos = gridBody.offsetTop;
-      gridBody.style.top = (currentPos + 50) + "px"; // down by 1px
-      checkCollision(interval);
-    }, 1000)
+  const gridFall = (intervalId) => {
+    const gridBodyPos = boardContainer.current.getBoundingClientRect();
+    const currentPos = gridBodyPos.top;
+    boardContainer.current.style.top = (currentPos - 55) + "px"; // moving grid down 5px
+    checkCollision(intervalId);
   }
 
   const checkCollision = (intervalId) => {
-    const gridBody = document.getElementById("boardrow-container")
-    const gridBodyPos = gridBody.getBoundingClientRect();
-    // console.log("GRIDBODY", gridBodyPos);
-    const dividerPos = document.getElementById("divider").getBoundingClientRect();
-    // console.log("DIVIDERPOS", dividerPos);
+    const gridBodyPos = boardContainer.current.getBoundingClientRect();
+    const dividerPos = dividerRef.current.getBoundingClientRect();
     if(gridBodyPos.bottom >= dividerPos.top) {
-      // gridBody.style.top = (gridBody.offsetTop + 67) + "px";
       clearInterval(intervalId);
       console.log("STOP");
     }
   }
 
   useEffect(() => {
-    gridFall();
-  }, [])
+    const intervalId = setInterval(() => {
+      gridFall(intervalId);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   useEffect(() => {
       // Grid update
@@ -47,7 +44,7 @@ function BoardContainer({input, inputRow, grid, setGrid}) {
   }, [input])
 
   return (
-      <div id="boardrow-container">
+      <div ref={boardContainer} id="boardrow-container">
         {[0,1,2,3,4].map((idx, index) => {
           return <BoardRow input={grid[index]}/>
         })
