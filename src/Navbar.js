4dc7d4d0b486620179cf08196ceebfe741fc09ea
Saba Zubair <zubairs19@protonmail.com>
2022-06-17 17:37:48 -0400

    removed unused code

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,7 @@
-import { AiOutlineQuestionCircle } from 'react-icons/ai'
-import {BiBarChartAlt2} from 'react-icons/bi';
-import {BsGearFill} from 'react-icons/bs';
-
 export default function Navbar() {
   return (
     <header id="header-container">
-      <h3 className="title">Wordle-anch!</h3>
+      <h3 className="title shimmer">Wordle-anch!</h3>
     </header>
   );
 }
