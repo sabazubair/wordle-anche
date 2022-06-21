8b338c943d7a63fd8a419a20f9b9f6574caedd40
Saba Zubair <zubairs19@protonmail.com>
2022-06-21 17:00:17 -0400

    used new field fontColor to toggle font color (white or black) based on whether user is inputting vs when that input is validated

--- src/BoardRow.js
+++ src/BoardRow.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import 'animate.css';
 
 export default function BoardRow({input}) {
   useEffect(() => {
@@ -8,8 +7,8 @@ export default function BoardRow({input}) {
   return (
     <div className="row">
       {input.map((sub) =>
-      <div style={{backgroundColor: sub.color, color: "#FFFFFF"}} className={`game-tile ` + (sub.letter != "" ? `animate__animated animate__pulse game-tile__submission` : "")}>
-        <p className="letter">{sub.letter}</p>
+      <div style={{backgroundColor: sub.color, borderColor: sub.fontColor ? sub.color : "none"}} className={`game-tile ` + (sub.letter != "" ? `game-tile__submission` : "")}>
+        <p className="letter" style={{color: sub.fontColor ? sub.fontColor : "#000000"}}>{sub.letter}</p>
       </div>)}
     </div>
   )
