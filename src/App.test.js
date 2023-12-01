62f160b35453cdd8428d3beab92104d2a8bb5c9b
Saba <46948491+sabazubair@users.noreply.github.com>
2023-12-01 16:05:01 -0500

    Wrote a first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // Import this for better matchers
+import App from "./App"; // Import your component
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
 });
