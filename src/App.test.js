f6f40d42eb2ba89f2cb7da37f162bde117e77b47
chintu1851 <115821667+chintu1851@users.noreply.github.com>
2023-12-13 18:56:07 -0500

    Add test cases in App.test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,38 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect"; // Import this for better matchers
-import App from "./App"; // Import your component
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
+import Navbar from "./Navbar"; // Check if the path to the Navbar component is correct
+import BoardRow from "./BoardRow";
+import Keyboard from './Keyboard';
 
 test("renders with a specific class name", () => {
-  // Render the component
   const { container } = render(<App />);
-
-  // Check if the element with the specified class exists
-  const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
+  const elementWithClassName = container.querySelector(".columns");
   expect(elementWithClassName).toBeInTheDocument();
 });
+
+test("renders Navbar", () => {
+  const { getByTestId } = render(<Navbar />);
+  const headerContainer = getByTestId("header-container");
+  expect(headerContainer).toBeInTheDocument();
+});
+
+test('Navbar title is rendered', () => {
+  const { getByText } = render(<Navbar />);
+  const titleElement = getByText('Wordle-anche!');
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders BoardRow without errors', () => {
+  const input = [
+    { letter: 'A', color: '#FFFFFF', fontColor: '#000000' },
+  ];
+  render(<BoardRow input={input} />);
+});
+
+test(' Keyboard running without problem', () => {
+  render(<Keyboard />);
+});
+
+
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <header id="header-container">
+    <header data-testid="header-container" id="header-container">
       <h3 className="title shimmer">Wordle-anche!</h3>
     </header>
   );
