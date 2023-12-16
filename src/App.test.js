5ed20f729ad9d967204c00bcd81eda1807932df9
payalpahilani <58146637+payalpahilani@users.noreply.github.com>
2023-12-16 12:18:06 -0500

    Added test to app.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,17 @@ import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // Import this for better matchers
 import App from "./App"; // Import your component
+import fs from 'fs';
+import path from 'path';
+import { screen } from '@testing-library/react';
+
+const htmlPath = path.join(__dirname, "../public/index.html");
+const htmlContent = fs.readFileSync(htmlPath, "utf-8");
+
+beforeEach(() => {
+  document.body.innerHTML = htmlContent;
+});
+
 
 test("renders with a specific class name", () => {
   // Render the component
@@ -10,4 +21,29 @@ test("renders with a specific class name", () => {
   // Check if the element with the specified class exists
   const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
   expect(elementWithClassName).toBeInTheDocument();
+});
+
+test('renders with the correct background color in body', () => {
+  render(<App />);
+  const bodyElement = document.body;
+  expect(bodyElement).toHaveStyle('background-color: rgb\\(250, 196, 209\\)');
+});
+
+test('renders with a specific title', () => {
+  const titleElement = document.querySelector('title');
+  expect(titleElement).toBeInTheDocument();
+  expect(titleElement.textContent).toBe('Wordle-anche!');
+});
+
+test('renders keyboard is present', () => {
+  render(<App />);
+  const keyboard = document.querySelector('.keyboard');
+  // Check if the element is present
+  expect(keyboard).toBeInTheDocument();
+});
+
+test('renders with the correct charset', () => {
+  render(<App />);
+  const charsetMeta = document.querySelector('meta[charset="utf-8"]');
+  expect(charsetMeta).toBeInTheDocument();
 });
