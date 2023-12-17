6ad3d3003547bd371347f9a58a0bd4641a3caf1c
Alaa <alaaeleraqui@gmail.com>
2023-12-17 18:44:00 -0500

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -9,5 +9,60 @@ test("renders with a specific class name", () => {
 
   // Check if the element with the specified class exists
   const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
-  expect(elementWithClassName).toBeInTheDocument();
+  expect(elementWithClassName).toBeInTheDocument()
+
+});
+
+//Test 1
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".board-col"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument()
+
+});
+
+//Test 2
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".letter"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument()
+});
+
+//Test 3
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".keyboard-wrapper"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument()
+
+});
+
+//Test 4
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".row"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument()
+
+});
+
+//Test 5
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".keyboard"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument()
+
 });
