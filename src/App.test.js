46d6f2a666e24081c6fcb3c45b20067203382488
manashishah <manashishah5@gmail.com>
2023-12-17 17:07:45 -0500

    wrote test cases

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // Import this for better matchers
 import App from "./App"; // Import your component
 
+test('renders without crashing', () => {
+  render(<App />);
+});
+
 test("renders with a specific class name", () => {
   // Render the component
   const { container } = render(<App />);
@@ -10,4 +14,22 @@ test("renders with a specific class name", () => {
   // Check if the element with the specified class exists
   const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
   expect(elementWithClassName).toBeInTheDocument();
+});
+
+test('renders the Wordle-anche! title', () => {
+  const { getByText } = render(<App />);
+  const titleElement = getByText(/Wordle-anche!/i);
+  expect(titleElement).toBeInTheDocument();
+});
+
+test('renders an hr tag with the id "divider"', () => {
+  render(<App />);
+  const dividerElement = document.getElementById('divider');
+  expect(dividerElement).toBeInTheDocument();
+});
+
+test('renders a div with the "board-col" class', () => {
+  render(<App />);
+  const columnsElements = document.getElementsByClassName('board-col');
+  expect(columnsElements.length).toBeGreaterThan(0);
 });
