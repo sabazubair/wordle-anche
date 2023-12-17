fdef5bc4ebafb897af8928fec418177ec383f2fd
ronanshakya <ronanshakya98@gmail.com>
2023-12-17 16:15:48 -0500

    Added 5 tests

--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,38 @@ test("renders with a specific class name", () => {
   const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
   expect(elementWithClassName).toBeInTheDocument();
 });
+
+test("renders the navbar", () => {
+  const { container } = render(<App />);
+
+  const elementWithId = container.querySelector("#header-container");
+  expect(elementWithId).toBeInTheDocument();
+});
+
+test("renders the navbar content", () => {
+  const { container } = render(<App />);
+
+  const titleElement = container.querySelector("#header-container");
+  expect(titleElement).toHaveTextContent("Wordle-anche!");
+});
+
+test("renders the keyboard correctly", () => {
+  const { container } = render(<App />);
+
+  const elementWithClassName = container.querySelector(".keyboard-wrapper");
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+test("renders the main container", () => {
+  const { container } = render(<App />);
+
+  const elementWithId = container.querySelector("#main");
+  expect(elementWithId).toBeInTheDocument();
+});
+
+test("renders a divider line", () => {
+  const { container } = render(<App />);
+
+  const elementWithId = container.querySelector("#divider");
+  expect(elementWithId).toBeInTheDocument();
+});
