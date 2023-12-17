673b64499f29dcb1aa2984d6f49177c9dcd19489
ujjwalahuja28 <uahuja08@gmail.com>
2023-12-17 02:17:11 -0500

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Hello
+clone the repository using this command:-
+git clone https://github.com/sabazubair/wordle-anche.git
+
+then opened the folder "wordle-anche" in VS code and run these commands in terminal:-
+npm install
+npm start
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,9 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect"; // Import this for better matchers
 import App from "./App"; // Import your component
 
+
+
+// Test 1
 test("renders with a specific class name", () => {
   // Render the component
   const { container } = render(<App />);
@@ -11,3 +14,54 @@ test("renders with a specific class name", () => {
   const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
   expect(elementWithClassName).toBeInTheDocument();
 });
+
+
+
+// Test 2
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".row"); // Replace 'example-class' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+
+// Test 3
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".keyboard-wrapper"); // Replace 'example-class' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+
+
+// Test 4
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".key"); // Replace 'example-class' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+
+
+
+// Test 5
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".letter"); // Replace 'example-class' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+
+
