065e8b1a2ad53843c5848184ba0056d423b9d387
neelp314918 <neelp314918@gmail.com>
2023-12-17 17:35:20 -0500

    edited 5 tests

--- src/App.test.js
+++ src/App.test.js
@@ -11,3 +11,39 @@ test("renders with a specific class name", () => {
   const elementWithClassName = container.querySelector(".columns"); // Replace 'your-class-name' with the actual class name
   expect(elementWithClassName).toBeInTheDocument();
 });
+
+test("renders with a specific Id", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithId = container.querySelector("#header-container"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithId).toBeInTheDocument();
+});
+
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".row"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
+
+test("renders with a specific Id", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithId = container.querySelector("#boardrow-container"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithId).toBeInTheDocument();
+});
+
+test("renders with a specific class name", () => {
+  // Render the component
+  const { container } = render(<App />);
+
+  // Check if the element with the specified class exists
+  const elementWithClassName = container.querySelector(".game-tile"); // Replace 'your-class-name' with the actual class name
+  expect(elementWithClassName).toBeInTheDocument();
+});
